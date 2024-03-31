f576a2506cdef591f07f1888003d1fb3c05d06a7
Nolubabalo Maxazana <nolubabalomaxazana@gmail..com>
2024-03-31 18:19:38 +0200

    quotes code week-3

---
+++ Week-3/stub-code/dd_gui.py
@@ -0,0 +1,19 @@
+from tkinter import *
+from tkinter import ttk
+
+class DailyDigestGUI:
+    pass # build the GUI
+
+"""
+The GUI should enable the admin to...
+    - configure which content sources to include in email
+    - add recipients
+    - remove recipients
+    - schedule daily time to send email
+    - configure sender credentials
+"""
+
+if __name__ == '__main__':
+    root = Tk()
+    app = DailyDigestGUI(root)
+    root.mainloop()
